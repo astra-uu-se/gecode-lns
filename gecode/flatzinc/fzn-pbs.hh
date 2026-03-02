bbf5d3dac9eea5b0309564207be5a83c365c71df
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2026-03-02 09:07:16 +0100

    5s timeout better

--- gecode/flatzinc/fzn-pbs.hh
+++ gecode/flatzinc/fzn-pbs.hh
@@ -528,7 +528,7 @@ private:
     long unsigned int _shavingStart{0};
     Support::Timer _timeout;
 
-    const double defaultTime{2000};
+    const double defaultTime{5000};
     std::optional<double> time;
     size_t _banditTimestamp{std::numeric_limits<size_t>::max()};
     size_t _numCurSolutions{0};
